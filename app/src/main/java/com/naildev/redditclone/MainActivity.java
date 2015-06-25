28a05e8cb2e2240288f5438cd6f62e67fdd986b5
coneill <coneill@guidewire.com>
2015-06-25 09:25:26 +0100

    OKHTTP Added

--- app/src/main/java/com/naildev/redditclone/MainActivity.java
+++ app/src/main/java/com/naildev/redditclone/MainActivity.java
@@ -2,15 +2,21 @@ package com.naildev.redditclone;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    public final String REDDIT_URL = "www.reddit.com/r/all.json?limit=5";
+    public final String REDDIT_URL = "http://www.reddit.com/r/all.json?limit=5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,23 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(REDDIT_URL)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                Log.d("RESPONSE", response.body().string());
+            }
+        });
+
 
     }
 
