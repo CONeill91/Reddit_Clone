ef884dffcbf425fa865e8fbdc57de99c4da37644
coneill <coneill@guidewire.com>
2015-06-25 09:45:26 +0100

    Google Volley Support for Request Queues

--- app/src/main/java/com/naildev/redditclone/MainActivity.java
+++ app/src/main/java/com/naildev/redditclone/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import java.io.IOException;
 
 
@@ -18,11 +21,41 @@ public class MainActivity extends ActionBarActivity {
 
     public final String REDDIT_URL = "http://www.reddit.com/r/all.json?limit=5";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView mTextView = (TextView) findViewById(R.id.text);
 
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, REDDIT_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
+
+
+    }
+
+
+}
+
+/*
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
@@ -40,9 +73,4 @@ public class MainActivity extends ActionBarActivity {
                 Log.d("RESPONSE", response.body().string());
             }
         });
-
-
-    }
-
-
-}
+*/
